6d044840906230f205167d346644fa251934dabe
VENUGOPAL <venumail13@gmail.com>
2024-08-03 13:36:29 +0530

    java8commit

---
+++ src/java8programs/java8basicdemo.java
@@ -0,0 +1,11 @@
+package java8programs;
+
+public class java8basicdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("java8basic programs");
+
+	}
+
+}
